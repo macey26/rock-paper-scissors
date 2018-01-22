90a991479e058fb1325a82e530e80fcd00573c6e
Emma Macey <macey26@hotmail.com>
2018-01-22 11:17:41 +1100

    Tidied up the code

--- console.js
+++ console.js
@@ -1,26 +1,23 @@
 let answer = ""
 
-//generates rock paper or scissors randomly from the computer
+
 let computerPlay = () => {
 	let comPick = ["rock", "paper", "scissors"];
 	let computer = comPick[Math.floor(Math.random() * comPick.length)];
 	return computer;
 };
 	
-// prompt the user to select rock paper or scissors		 
+	 
 let userPlay = () => {
 	let player = prompt("please enter your choice of rock paper scissors").toLowerCase();
 	return player;
 };
 
-// creates a round of rock paper scissors to return the answer 
 let playRound = (playerSelection, computerSelection) => {
 
     console.log("Player chose " + playerSelection);
 	console.log("Computer chose " + computerSelection);
 	
-
-
     switch(playerSelection){
 	    case "rock" :
 		    if (computerSelection === "scissors"){
@@ -65,23 +62,23 @@ let playRound = (playerSelection, computerSelection) => {
 	    break;
 
 	    default:
-	    answer = "something went wrong"
-	}
+	    answer = "something went wrong";
+	};
 
 	 return answer;
 }; 
 
-// runs 5 rounds of rock paper scissors and returns the winner
-  let game = () => {
-       let playerScore = 0;
-       let computerScore = 0;
-       let rounds = 5;
+
+let game = () => {
+    let playerScore = 0;
+    let computerScore = 0;
+    let rounds = 5;
 
        for (let i = 0; i < rounds; i++) {
 
        	let computerSelection = computerPlay();
         let playerSelection = userPlay();
-console.log(playRound(playerSelection, computerSelection));
+            console.log(playRound(playerSelection, computerSelection));
 
 
        	if (answer === "Computer WINS!!!"){
@@ -96,23 +93,21 @@ console.log(playRound(playerSelection, computerSelection));
        		console.log(playerScore );
        	} else {
 
-       		console.log("You tied");
+       		 console.log("You tied");
        	}
 
        	console.log(playerScore, computerScore);
        
-       }
+       };
 
        if (playerScore > computerScore){
            
            alert( "Give the player a medal");
        } else if(playerScore < computerScore){
 
-       	   alert(" Give the Computer a medal")
-       	}else{
+       	   alert(" Give the Computer a medal");
+       }else{
 
-       		alert("Its a Tie, you both get a medal")
-       	}
-  }
-
- ; 
+       		alert("Its a Tie, you both get a medal");
+       };
+  }; 
